3f6c898b9a342f950849736e9899ead2ba2229c8
Ferney <ferney.montoyag@gmail.com>
2024-12-13 09:53:45 -0500

    :sparkles: UI password recovery created with its validations in zod and linked with react hook form

---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import ForgotPassword from "@/modules/auth/pages/ForgotPassword";
+
+const page = () => {
+  return (
+    <div className="grid place-items-center mt-10">
+      <ForgotPassword />
+    </div>
+  );
+};
+
+export default page;

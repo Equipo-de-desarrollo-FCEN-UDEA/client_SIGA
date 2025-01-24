2f8b9c38f4d33e67304529c2588758f887c2423e
estebandurangov <esteban.durangov@udea.edu.co>
2025-01-24 13:54:26 -0500

    Refactor API endpoints for academic units, roles, and users

--- src/core/services/api/academicUnitService.ts
+++ src/core/services/api/academicUnitService.ts
@@ -2,7 +2,7 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 export const fetchAcademicUnits = async () => {
   try {
     const academic_unit_response = await fetch(
-      apiUrl+"/api/v1/academic_unit/get-all?skip=0&limit=50"
+      apiUrl+"/academic_unit/get-all?skip=0&limit=50"
     );
     const data_academic_unit = await academic_unit_response.json();
 
--- src/core/services/api/rolService.ts
+++ src/core/services/api/rolService.ts
@@ -2,7 +2,7 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 export const fetchRoles = async () => {
   try {
     const response_roles = await fetch(
-      apiUrl+"/api/v1/rol/get-all?skip=0&limit=10"
+      apiUrl+"/rol/get-all?skip=0&limit=10"
     );
     const data_roles = await response_roles.json();
 

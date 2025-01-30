a54be9e1c4843773759695a12ed35decd7789002
estebandurangov <esteban.durangov@udea.edu.co>
2025-01-30 13:30:37 -0500

    Refactor Subjects component to improve accessibility and user experience

--- src/modules/applications/mobility/components/atoms/Subjects.tsx
+++ src/modules/applications/mobility/components/atoms/Subjects.tsx
@@ -114,7 +114,13 @@ const Subjects = ({
 
       <div
         onClick={() => setModal(true)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+        setModal(true);
+          }
+        }}
         role="button"
+        tabIndex={0}
         className="bg-green-500 text-white cursor-pointer mt-16 px-4 py-2 rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex items-center justify-center"
       >
         Agregar Materia <CiSquarePlus className="ml-2 text-lg" />

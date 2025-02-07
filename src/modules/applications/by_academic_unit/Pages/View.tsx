60b959cabfaeebbb4a73901ad972b735f636160f
estebandurangov <esteban.durangov@udea.edu.co>
2025-02-06 19:23:24 -0500

    Refactor View component to include Table component for displaying user applications by academic unit

--- src/modules/applications/by_academic_unit/Pages/View.tsx
+++ src/modules/applications/by_academic_unit/Pages/View.tsx
@@ -1,29 +1,48 @@
 "use client";
-import UserApplication from "@/core/interfaces/userApplication";
+import UserApplicationAcademicUnit from "@/core/interfaces/applications/userApplicationAcademicUnit";
 import UserApplicationAcademicUnitService from "@/core/services/api/applications/user_application_academic_unit";
 import { useEffect, useState } from "react";
+import Table from "@/components/organisms/Table";
 
+type list = string[];
 
 function Page({ id }: { id: string }) {
-    const [userApplications, setUserApplication] = useState<UserApplication | null>(null);
+    const [userApplicationsAcademicUnit, setUserApplicationAcademicUnit] = useState<UserApplicationAcademicUnit[] | null>(null);
+    const [rows, setRows] = useState<list[]>([]);
+    const headers = ['Solicitante', 'Tipo', 'Estado', ''];
 
     useEffect(() => {
         const us_app_academic_unit = new UserApplicationAcademicUnitService();
         const fetchData = async () => {
             try {
                 const data = await us_app_academic_unit.getUserApplicationAcademicUnitByAcademicUnit(id);
-                setUserApplication(data);
+                setUserApplicationAcademicUnit(data);
             } catch (err) {
-                console.error('Error fetching data:', err);
+                console.error("Error fetching data", err);
             }
         };
         fetchData();
-        console.log(userApplications)
     }, [id]);
-    
+
+    // Nuevo useEffect que se activa cuando userApplicationsAcademicUnit cambia
+    useEffect(() => {
+        if (userApplicationsAcademicUnit) {
+            const newRows = userApplicationsAcademicUnit.map((userApplicationAcademicUnit) => [
+                userApplicationAcademicUnit.user_application.user.name,
+                userApplicationAcademicUnit.user_application.application.name,
+                userApplicationAcademicUnit.is_active ? 'Activo' : 'Inactivo',
+                ""
+            ]);
+            setRows(newRows);
+        }
+    }, [userApplicationsAcademicUnit]);
+
     return (
         <>
-            <p>Info de las movilidades</p>
+            <h1>userApplications</h1>
+            <div>
+                <Table headers={headers} rows={rows} />
+            </div>
         </>
     );
 }

0d901ee3b7f3a11088f81953f5de7aad03a7a27a
estebandurangov <esteban.durangov@udea.edu.co>
2025-02-06 19:22:48 -0500

    Refactor userApplication.ts and add UserApplicationAcademicUnit interface

--- src/core/interfaces/applications/userApplication.ts
+++ src/core/interfaces/applications/userApplication.ts
@@ -2,11 +2,7 @@ import User from "@/core/interfaces/user";
 import Application from "@/core/interfaces/applications/application";
 
 interface UserApplication {
-    user_id: string;
-    application_id: string;
     id: string;
-    created_at: string;
-    updated_at: string;
     application: Application;
     user: User;
 }
---
+++ src/core/interfaces/applications/userApplicationAcademicUnit.ts
@@ -0,0 +1,8 @@
+import UserApplication from "./userApplication";
+
+interface UserApplicationAcademicUnit {
+    is_active: boolean;
+    user_application: UserApplication;
+}
+
+export default UserApplicationAcademicUnit;

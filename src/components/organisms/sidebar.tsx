3e6eeb476a89838f63e27b0d375f11b25948e8be
estebandurangov <esteban.durangov@udea.edu.co>
2025-02-12 19:02:53 -0500

    - Se crea estilo base para sidebar, - se agrega Icono de perfil - se agrega Boton de SignOut

--- src/components/organisms/sidebar.tsx
+++ src/components/organisms/sidebar.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from 'react'
 import { useSession } from '@/core/providers/SessionProvider'
+import { PiSignOutBold } from "react-icons/pi";
+import { RiShieldUserLine } from "react-icons/ri";
 
 const Sidebar = () => {
     const { user } = useSession();
@@ -8,8 +10,18 @@ const Sidebar = () => {
         return (
             <div className='flex bg-gray w-[350px] shadow-sm shadow-gray-600'>
                 <aside className="flex-1 h-screen sticky top-0">
-                    {user.name}
-                    <p>{user.last_name}</p>
+                    <div className="flex flex-col mx-auto items-center gap-1 my-8">
+                        <RiShieldUserLine size={100} color='green' />
+                        <span className='font-thin text-gray-800 tracking-[0.1em] pt-2'>{user.name} {user.last_name}</span>
+                        <div className='flex items-center gap-x-2 cursor-pointer'>
+                            <p>Cerrar Sesión</p>
+                            <PiSignOutBold
+                                onClick={() => signOut()}
+                            />
+                        </div>
+                        
+
+                    </div>
                 </aside>
             </div>
           )

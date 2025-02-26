c2b5c67c88d818a0cf44335f431c6dd8b6b271a6
DavidTorres-sys <david.torresg@udea.edu.co>
2025-02-26 09:21:36 -0500

    Refactor commission form component by removing unused imports and state management

--- src/modules/applications/commission/pages/Form.tsx
+++ src/modules/applications/commission/pages/Form.tsx
@@ -1,16 +1,12 @@
-
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 import FormStepper from "@/components/molecules/FormStepper/FormStepper";
-import { FormProvider, useForm } from "react-hook-form";
 import { combinedSchema } from "@/core/schemas/mobilityCreateFormSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useStepperForm } from "@/core/hooks/useStepperForm";
-import Subject from "@/core/interfaces/applications/mobility/subject";
 import Place from "../components/Place";
+import { useForm, FormProvider } from "react-hook-form";
 
 const FormCommission = () => {
-  const router = useRouter();
   const methods = useForm<any>({
     resolver: zodResolver(combinedSchema),
   });
@@ -19,8 +15,6 @@ const FormCommission = () => {
       methods,
       combinedSchema,
   });
-
-  const [subjects, setSubjects] = useState<Subject[]>([]);
   
   const onSubmit = async (data: any) => {
     console.log(data);

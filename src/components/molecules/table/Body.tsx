95dc08727ffbeb904948f029500c9cfc6dc87117
estebandurangov <esteban.durangov@udea.edu.co>
2025-02-27 10:21:44 -0500

    Refactorizar componentes Table para recibir un link y mostrar la solicitud segun su tipo

--- src/components/molecules/table/Body.tsx
+++ src/components/molecules/table/Body.tsx
@@ -1,23 +1,26 @@
 "use client";
 import React from 'react';
 
-type list = string[];
 interface BodyProps {
-    rows: list[];
+    rows: string[][];
+    link?: boolean;
 }
 
-const Body: React.FC<BodyProps> = ({ rows }) => {
-    return (
-        <tbody>
-            {rows.map((row, index) => (
-                <tr key={index}>
-                    {row.map((cell) => (
-                        <td key={cell} className="px-6 py-4 border-b border-gray-300">{cell}</td>
-                    ))}
-                </tr>
-            ))}
-        </tbody>
-    );
-}
+const Body = ({ rows, link }: BodyProps) => (
+    <tbody>
+        {rows.map((row) => (
+            <tr key={row[row.length-1]}>
+                {row.slice(0, -1).map((cell) => (
+                    <td key={cell}>{cell}</td>
+                ))}
+                {link && (
+                    <td>
+                        <a href={row[row.length - 1]}>Ver detalles</a>
+                    </td>
+                )}
+            </tr>
+        ))}
+    </tbody>
+);
 
 export default Body;

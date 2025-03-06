3e0048a1a4a8de8a6d6a35c9f6439dbb12d6ea28
alwos19 <jhon.jaramilloe@udea.edu.co>
2025-03-06 15:56:02 -0500

    conflicts

---
+++ src/app/solicitudes/commission/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import FormCommission from "@/modules/applications/commission/pages/Form";
+
+// Definir tipos para los datos del formulario
+const CommissionCreate: React.FC = () => {
+    return (
+      <FormCommission />
+    );
+};
+
+export default CommissionCreate;

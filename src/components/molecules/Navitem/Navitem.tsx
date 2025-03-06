e3812f947e8cbbc8ef3ba4ded0f981f96fb00d64
jeronimoloaiza <jeronimo.loaiza@udea.edu.co>
2025-03-06 16:39:07 -0500

    :sparkles: Primera version sidebar

---
+++ src/components/molecules/Navitem/Navitem.tsx
@@ -0,0 +1,95 @@
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { LuShoppingCart } from "react-icons/lu";
+import { IoEarthOutline } from "react-icons/io5";
+import * as Tooltip from "@radix-ui/react-tooltip";
+import React from "react";
+
+
+export const NavItems = () => {
+    const pathname = usePathname();
+
+    function inNavItemActive(pathname: string, nav: string) {
+        return pathname.includes(nav);
+    }
+    return [
+        // {
+        //     title: "Home",
+        //     href: "/",
+        //     icon: "",
+        //     position: "top",
+        //     active: inNavItemActive(pathname, "/"),
+        // },
+        // {
+        //     title: "Perfil",
+        //     href: "/",
+        //     icon: "",
+        //     position: "top",
+        //     active: inNavItemActive(pathname, "/"),
+        // },
+        {
+            title: "Movilidad",
+            href: "/solicitudes/movilidad/create",
+            icon: <IoEarthOutline size ={30} color="green"/>,
+            position: "top",
+            active: inNavItemActive(pathname, "/movilidad/create"),
+        },
+        {
+            title: "Compras",
+            href: "/solicitudes/compras/crear",
+            icon: <LuShoppingCart size ={30} color="green"/>,
+            position: "top",
+            active: inNavItemActive(pathname, "/compras/crear"),
+        }
+    ]
+}
+
+export const SideNavItem: React.FC<{
+    label: string;
+    path: string;
+    icon: React.ReactNode;
+    active: boolean;
+    isSidebarExpanded: boolean;
+}> = ({ label, path, icon, active, isSidebarExpanded }) => (
+        <>
+            {isSidebarExpanded ? (
+                <Link 
+                    href={path}
+                    className={`flex items-center gap-2 p-2 rounded-lg`}
+                >
+                    <div className="flex items-center justify-center w-8 h-8">
+                        <span className="flex gap-3 items-end font-medium hover:text-green-800 ">
+                            {icon} {label}
+                        </span>
+                    </div>
+                </Link>
+            ) : (
+                <Tooltip.Provider delayDuration={70}>
+                    <Tooltip.Root>
+                      <Tooltip.Trigger asChild>
+                        <Link
+                          href={path}
+                          className={`h-full relative flex items-center whitespace-nowrap rounded-md ${
+                            active ? "bg-gray-200 text-gray-900" : "text-gray-600 hover:bg-gray-200"
+                          }`}
+                        >
+                          <div className="flex items-center justify-center w-8 h-8">
+                            {icon}
+                          </div>
+                        </Link>
+                      </Tooltip.Trigger>
+                      <Tooltip.Portal>
+                        <Tooltip.Content
+                          side="left"
+                          sideOffset={10}
+                          className="bg-gray-800 text-white text-xs px-3 py-1.5 rounded-md shadow-md"
+                        >
+                          {label}
+                          <Tooltip.Arrow className="fill-gray-800" />
+                        </Tooltip.Content>
+                      </Tooltip.Portal>
+                    </Tooltip.Root>
+                </Tooltip.Provider>
+            )}
+        </>
+    )
--- src/components/organisms/sidebar.tsx
+++ src/components/organisms/sidebar.tsx
@@ -1,17 +1,11 @@
 "use client";
-import React, {useState} from 'react'
+import React, { Fragment, useState } from 'react'
 import { useSession } from '@/core/providers/SessionProvider'
-import { PiSignOutBold } from "react-icons/pi";
+import { PiSignOutBold, } from "react-icons/pi";
 import { RiShieldUserLine } from "react-icons/ri";
+import { IoIosArrowDown } from "react-icons/io";
 import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
-import { NavItems } from '@/components/molecules/Routes/routes'
-// import { cn } from '@/lib/utils'
-// import {
-//     Tooltip,
-//     TooltipContent,
-//     TooltipProvider,
-//     TooltipTrigger,
-// } from '@/components/ui/tooltip'
+import { NavItems, SideNavItem } from '@/components/molecules/Navitem/Navitem'
 
 const Sidebar = () => {
     const { user, logout: signOut } = useSession();
@@ -25,41 +19,74 @@ const Sidebar = () => {
             <div className={`flex bg-gray-50 shadow-sm shadow-gray-600 
               ${isSidebarExpanded ? "w-[250px]" : "w-[68px]"}
               transition-all duration-300 ease-in-out`}>
-                <aside className="flex-1 h-screen sticky top-0">
-                    <div className="flex flex-col mx-auto items-center gap-1 my-8">
-                        <RiShieldUserLine size={100} color='green' />
-                        <span className='font-thin text-gray-800 tracking-[0.1em] pt-2'>{user.name} {user.last_name}</span>
-                        <div className='flex items-center gap-x-2 cursor-pointer' onClick={signOut}>
-                            <p>Cerrar Sesión</p>
-                            <PiSignOutBold />
-                        </div>
-                        <button 
-                            className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+                <aside className="h-screen w-[250px] bg-gray-50 border-r border-gray-300 
+                                  flex flex-col justify-between p-4 shadow-sm">
+
+                    <div className={`flex flex-col gap-3 transition-all duration-300 ease-in-out items-center`}>
+                        <RiShieldUserLine size={60} color="green" />
+                        <span className="font-semibold text-gray-800 tracking-wide">
+                            {user.name} {user.last_name}
+                        </span>
+                        <button
+                            className={`flex items-center gap-2 
+                                        text-gray-800 px-4 py-2
+                                        font-semibold rounded-lg 
+                                        hover:bg-gray-200 transition-all ${!isSidebarExpanded ? "justify-center w-15 h-15 p-0" : ""
+                                }`}
                             onClick={() => setShowOptions(!showOptions)}
                         >
-                            Realizar solicitud
+                            {isSidebarExpanded && <span>Realizar solicitud</span>}
+                            <IoIosArrowDown size={20} />
                         </button>
+
+                        {/* Sección de opciones, solo se muestra si showOptions es true */}
+                        {showOptions && (
+                            <div className="relative pb-4">
+                                <div className="flex flex-col space-y-3">
+                                    {navItems.map((item) => (
+                                        <SideNavItem
+                                            key={item.href}
+                                            label={item.title}
+                                            path={item.href}
+                                            icon={item.icon}
+                                            active={item.active}
+                                            isSidebarExpanded={isSidebarExpanded}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                        )}
                     </div>
+                    <button
+                        className={`flex items-center rounded-lg transition-all text-gray-800 
+                                      hover:bg-gray-200 w-full ${isSidebarExpanded ? "px-4 py-2 gap-2 justify-center" : "w-12 h-12 p-0 justify-center"
+                            }`}
+                        onClick={signOut}
+                    >
+                        <PiSignOutBold size={20} color="red" />
+                        {isSidebarExpanded && <span>Cerrar Sesión</span>}
+                    </button>
                 </aside>
+
                 <div className="relative">
-                  <button
-                    type="button"
-                    className="absolute bottom-40 right-[-12px] flex h-10 w-10 items-center 
+                    <button
+                        type="button"
+                        className="absolute bottom-40 right-[-12px] flex h-10 w-10 items-center 
                                justify-center rounded-full bg-green-700 text-white 
                                shadow-lg hover:bg-green-600 hover:shadow-xl 
                                transition-all duration-200 ease-in-out"
-                    onClick={toggleSidebar}
-                  >
-                    {isSidebarExpanded ? (
-                      <FaChevronCircleLeft size={24} />
-                    ) : (
-                      <FaChevronCircleRight size={24} />
-                    )}
-                  </button>
+                        onClick={toggleSidebar}
+                    >
+                        {isSidebarExpanded ? (
+                            <FaChevronCircleLeft size={24} />
+                        ) : (
+                            <FaChevronCircleRight size={24} />
+                        )}
+                    </button>
                 </div>
 
             </div>
-          )
+        )
     }
 }
 

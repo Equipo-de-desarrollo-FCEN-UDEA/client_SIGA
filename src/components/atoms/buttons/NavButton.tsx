be5a6057d6a45f7386339bf07048d7eb5cdc0134
jeronimoloaiza <jeronimo.loaiza@udea.edu.co>
2025-03-10 16:08:20 -0500

    :zap: Sidebar lista con botón de Home. Se modulariza el NavButton que permite que cuando solo se muestre el ícono de un botón, muestre el nombre pequeñito al pasar el mouse.

---
+++ src/components/atoms/buttons/NavButton.tsx
@@ -0,0 +1,75 @@
+import Link from "next/link";
+import * as Tooltip from "@radix-ui/react-tooltip";
+
+interface NavButtonProps {
+    label: string;
+    href?: string; // Si es un botón, href será opcional
+    icon?: React.ReactNode;
+    isActive?: boolean;
+    isExpanded?: boolean;
+    iconPosition?: "left" | "right";
+    className?: string;
+    onClick?: () => void; // Para botones sin navegación
+}
+
+export const NavButton: React.FC<NavButtonProps> = ({
+    label,
+    href,
+    icon,
+    isActive = false,
+    isExpanded = true,
+    iconPosition = "left",
+    onClick,
+    className = "",
+}) => {
+    const content = (
+        <button
+            onClick={onClick}
+            className={`flex items-center w-full px-4 py-2 rounded-lg transition
+                        ${isActive ? "text-green-700 font-semibold hover:bg-gray-200" : "hover:bg-gray-200 text-gray-800"}
+                        ${isExpanded ? "justify-start gap-3 hover:bg-gray-200" : "justify-center w-12 h-12"}
+                        ${className}
+            `}
+        >
+            {iconPosition === "left" && icon}
+            {isExpanded && <span>{label}</span>}
+            {iconPosition === "right" && icon}
+        </button>
+    );
+
+    return isExpanded ? (
+        href ? (
+            <Link href={href} className="w-full">
+                {content}
+            </Link>
+        ) : (
+            content
+        )
+    ) : (
+        <Tooltip.Provider delayDuration={70}>
+            <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                    {href ? (
+                        <Link href={href} className="w-full flex justify-center">
+                            {content}
+                        </Link>
+                    ) : (
+                        <button className="w-full flex justify-center" onClick={onClick}>
+                            {content}
+                        </button>
+                    )}
+                </Tooltip.Trigger>
+                <Tooltip.Portal>
+                    <Tooltip.Content
+                        side="left"
+                        sideOffset={10}
+                        className="bg-gray-800 text-white text-xs px-3 py-1.5 rounded-md shadow-md"
+                    >
+                        {label}
+                        <Tooltip.Arrow className="fill-gray-800" />
+                    </Tooltip.Content>
+                </Tooltip.Portal>
+            </Tooltip.Root>
+        </Tooltip.Provider>
+    );
+};
--- src/components/organisms/sidebar.tsx
+++ src/components/organisms/sidebar.tsx
@@ -3,99 +3,103 @@ import React, { useState } from 'react'
 import { useSession } from '@/core/providers/SessionProvider'
 import { PiSignOutBold, } from "react-icons/pi";
 import { RiShieldUserLine } from "react-icons/ri";
-import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
-import { NavItems, SideNavItem } from '@/components/molecules/Navitem/Navitem';
-import * as Tooltip from "@radix-ui/react-tooltip";
+import { NavApplications, NavLinks, SideNavItem } from '@/components/molecules/Navitem/Navitem';
+import { NavButton } from '../atoms/buttons/NavButton';
 
 const Sidebar = () => {
     const { user, logout: signOut } = useSession();
     const [showOptions, setShowOptions] = useState(false);
     const [isSidebarExpanded, setIsSidebarExpanded] = useState(true);
-    const navItems = NavItems();
+    const navApplications = NavApplications();
+    const navLinks = NavLinks();
     const toggleSidebar = () => setIsSidebarExpanded(!isSidebarExpanded);
 
     if (user != null) {
         return (
             <div className={`flex 
-              ${isSidebarExpanded ? "w-[230px]" : "w-[68px]"}
+              ${isSidebarExpanded ? "w-[230px]" : "w-[101px]"}
               transition-all duration-300 ease-in-out`}>
                 <aside className="h-screen w-[230px] bg-gray-50 border-r border-gray-300 
                                   flex flex-col justify-between p-4 shadow-sm shadow-gray-600">
 
-                    <div className={`flex flex-col gap-3 transition-all duration-300 ease-in-out items-center`}>
+                    <div className={`flex flex-col gap-2 transition-all duration-300 ease-in-out items-center`}>
                         <RiShieldUserLine size={60} color="green" />
                         <span className="font-semibold text-gray-800 tracking-wide">
                             {user.name} {user.last_name}
                         </span>
-                        <button
-                            className={`flex items-center w-full px-4 py-2 rounded-lg font-semibold
-                                        text-gray-800 hover:bg-gray-200 transition-all 
-                                        ${isSidebarExpanded ? "justify-center" : "justify-center w-15 h-15 p-0"}
-                                        ${showOptions ? "text-green-700 bg-gray-200" : ""}
-                            `}
-                            onClick={() => setShowOptions(!showOptions)}
-                        >
-                            {isSidebarExpanded ? (
-                                <span className="w-full flex items-center justify-center gap-2">
-                                    Crear solicitud
-                                    <IoIosArrowDown size={20} />
-                                </span>
-                            ) : (
-                                <Tooltip.Provider delayDuration={70}>
-                                    <Tooltip.Root>
-                                        <Tooltip.Trigger asChild>
-                                            <button className="flex items-center justify-center rounded-full hover:bg-gray-200 transition">
-                                                <IoIosArrowDown size={25} />
-                                            </button>
-                                        </Tooltip.Trigger>
-                                        <Tooltip.Portal>
-                                            <Tooltip.Content
-                                                side="left"
-                                                sideOffset={10}
-                                                align="center"
-                                                className="bg-gray-800 text-white text-xs px-3 py-1.5 rounded-md shadow-md"
-                                            >
-                                                Crear solicitud
-                                                <Tooltip.Arrow className="fill-gray-800" />
-                                            </Tooltip.Content>
-                                        </Tooltip.Portal>
-                                    </Tooltip.Root>
-                                </Tooltip.Provider>
-                                
-                            )}
-                        </button>
 
-                        {/* Sección de opciones, solo se muestra si showOptions es true */}
-                        {showOptions && (
-                            <div className="relative pb-4">
-                                <div className="flex flex-col space-y-3">
-                                    {navItems.map((item) => (
-                                        <SideNavItem
-                                            key={item.href}
-                                            label={item.title}
-                                            path={item.href}
-                                            icon={item.icon}
-                                            active={item.active}
-                                            isSidebarExpanded={isSidebarExpanded}
-                                        />
-                                    ))}
-                                </div>
+                        <div className="w-full border-b border-gray-300 pb-4"></div>
+
+                        {/*Botones de Home, perfil...*/}
+                        <div className="relative pb-4 w-full">
+                            <div className="flex flex-col items-start w-full">
+                                {navLinks.map((item) => (
+                                    <SideNavItem
+                                        key={item.href}
+                                        label={item.title}
+                                        path={item.href}
+                                        icon={item.icon}
+                                        active={item.active}
+                                        isSidebarExpanded={isSidebarExpanded}
+                                    />
+                                ))}
                             </div>
-                        )}
+                        </div>
 
+                        <div className="w-full border-b border-gray-300 pb-4"></div>
+
+                        {/*Botón de crear solicitud*/}
+                        <div className="items-start w-full">
+                            <NavButton
+                                label="Crear solicitud"
+                                icon={showOptions ? <IoIosArrowUp size={isSidebarExpanded ? 20 : 25} />
+                                    : <IoIosArrowDown size={isSidebarExpanded ? 20 : 25} />}
+                                isActive={showOptions}
+                                isExpanded={isSidebarExpanded}
+                                iconPosition="right"
+                                onClick={() => setShowOptions(!showOptions)}
+                                className={`bg-gray-50 ${isSidebarExpanded ? "justify-between" : "justify-center"}`}
+                            />
+
+
+                            {/* Solicitudes, solo se muestra si showOptions es true */}
+                            {showOptions && (
+                                <div className="relative pb-4 w-full">
+                                    <div className="flex flex-col items-start w-full">
+                                        {navApplications.map((item) => (
+                                            <SideNavItem
+                                                key={item.href}
+                                                label={item.title}
+                                                path={item.href}
+                                                icon={item.icon}
+                                                active={item.active}
+                                                isSidebarExpanded={isSidebarExpanded}
+                                                className={"text-sm"}
+                                            />
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
                     </div>
-                    <button
-                        className={`flex items-center rounded-lg transition-all text-gray-800 
-                                      hover:bg-gray-200 w-full ${isSidebarExpanded ? "px-4 py-2 gap-2 justify-center" : "w-12 h-12 p-0 justify-center"
-                            }`}
+
+                    {/* Botón de cerrar sesión*/}
+                    <NavButton
+                        label="Cerrar Sesión"
+                        icon={<PiSignOutBold size={25} color="red" />}
+                        isActive={false}
+                        isExpanded={isSidebarExpanded}
+                        iconPosition="left"
                         onClick={signOut}
-                    >
-                        <PiSignOutBold size={20} color="red" />
-                        {isSidebarExpanded && <span>Cerrar Sesión</span>}
-                    </button>
+                        className={`hover:bg-gray-200 text-gray-800 w-full ${isSidebarExpanded ? "px-4 py-2 gap-2 justify-start"
+                            : "w-12 h-12 p-0 justify-center"}
+                            `}
+                    />
                 </aside>
 
+                {/*Botón para expandir o contraer el sidebar*/}
                 <div className="relative">
                     <button
                         type="button"

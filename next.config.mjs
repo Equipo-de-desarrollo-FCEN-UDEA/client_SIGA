6cc726563db213005bcfe793cbd4eff6116b3523
alwos19 <jhon.jaramilloe@udea.edu.co>
2025-04-09 18:22:42 -0500

    fix: add dotenv configuration to load environment variables from prod.env

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv';
+
+// Cargar las variables desde el archivo prod.env
+dotenv.config({ path: './prod.env' });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 

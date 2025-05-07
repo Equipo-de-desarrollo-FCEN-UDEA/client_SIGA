2d0c01c4452ac62d986ed32f2a4c0d82ed62518b
alwos19 <jhon.jaramilloe@udea.edu.co>
2025-05-07 10:25:00 -0500

    fix: update environment variable loading in next.config.mjs and correct Dockerfile.build paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
 
-// Cargar las variables desde el archivo prod.env
-dotenv.config({ path: '.env.prod' });
+// Cargar las variables desde el archivo .env
+dotenv.config();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    SECRET_KEY: process.env.SECRET_KEY,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+  },
+};
 
 export default nextConfig;

abc17196c28951c5de3997244c0de39f27eca1b3
alwos19 <jhon.jaramilloe@udea.edu.co>
2025-05-07 10:07:04 -0500

    fix: update dotenv configuration to use the correct .env.prod file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 // Cargar las variables desde el archivo prod.env
-dotenv.config({ path: './prod.env' });
+dotenv.config({ path: '.env.prod' });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
